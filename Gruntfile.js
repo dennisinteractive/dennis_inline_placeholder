780298a2316a23d3e73c4968116c8eff754afc73
Matt Coleman <hello@matt-coleman.co.uk>
2016-06-06 15:34:37 +0100

    Removed requireJS from Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,42 +36,6 @@ module.exports = function(grunt) {
       dist: ['Gruntfile.js', 'js/**/*.js']
     },
 
-    requirejs: {
-      compile: {
-        options: {
-          baseUrl: 'js/src',
-          paths: {
-            dfpinline: '.',
-            domReady: '<%= meta.distroPath %>/libraries/domreadyjs/domReady',
-            has: '<%= meta.distroPath %>/modules/custom/dennis_js/js/has',
-            utils: '<%= meta.distroPath %>/modules/custom/dennis_js/js/utils',
-            support: '<%= meta.distroPath %>/modules/custom/dennis_js/js/support',
-
-            // Stub modules
-            'jquery': 'empty:',
-            'jquery.once': 'empty:',
-            'drupal': 'empty:',
-            'googletag': 'empty:'
-          },
-          modules: [
-            { name: '<%= meta.dist.outfile %>' }
-          ],
-
-          dir: 'js/dist',
-          enforceDefine: false,
-          findNestedDependencies: true,
-          preserveLicenseComments: false,
-          generateSourceMaps: false,
-          optimize: 'uglify2' // 'uglify2' or 'none'
-        }
-      }
-    },
-
-    hashres: {
-      options: {
-        // This filename format is looked up in _dennis_js_get_dist_path().
-        fileNameFormat: '${name}.${hash}.${ext}',
-      },
       dist: {
         src: ['js/dist/<%= meta.dist.outfile %>.js'],
         dest: []
@@ -97,7 +61,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-bytesize');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-requirejs');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-hashres');
 
