7de1acd28937f039b7b74a15e2e8de4d2799fbbe
Chris kinch <christopher_kinch@dennis.co.uk>
2016-08-17 16:46:39 +0100

    Case 26558; Temp fix for GCS. Forced to only show on viewports less then 760.

--- js/src/app.js
+++ js/src/app.js
@@ -15,7 +15,14 @@ var dennisDfpInline = dennisDfpInline || {};
   Drupal.behaviors.dennisDfpInline = {
     attach: function( context, settings ) {
 
-      if ( window.googletag &&
+      // Chris K - Temp fix.
+      // Google Consumer Surveys break when used in conjunction with Dennis Inline Placeholders.
+      // Until we refactor this to be PHP/Drupal based this fix forces placeholders on mobile only
+      // where GCS are not required.
+      var vw = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+
+      if ( vw < 760 &&
+          window.googletag &&
           Drupal.settings.dennisDfpInline &&
           Drupal.settings.dennisDfpInline.config &&
           Drupal.settings.dennisDfpInline.config.selector
